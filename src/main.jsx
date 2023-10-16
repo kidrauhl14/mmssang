3c9553f0b09ae69d0b622660eb27ca6072af97ed
Eunchae <ddungal3@gmail.com>
2023-10-16 16:44:36 +0900

    Initialize React+JS project using Vite

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// index.jsx
+// app.jsx와 index.html을 이어준다.
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
