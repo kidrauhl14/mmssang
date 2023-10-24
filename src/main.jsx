3d3012b7a4e4f6f2db726344338632fbf87f2ac3
Eunchae <ddungal3@gmail.com>
2023-10-24 12:54:01 +0900

    Chore: Firebase 세팅하기 (Firebase SDK 추가)

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import {BrowserRouter as Router} from "react-router-dom";
+import firebase from "./firebaseApp.js";
+
+console.log(firebase);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
