a435b3117f13ef4bafc95b86c98f6f09130a5436
Eunchae <ddungal3@gmail.com>
2023-10-31 12:43:08 +0900

    Feat: [PostDetail] 고객센터게시판 상세글 페이지 마크업, scss mixin 생성

--- src/components/postdetail/index.jsx
+++ src/components/postdetail/index.jsx
@@ -2,11 +2,22 @@ import './index.scss';
 
 export default function PostDetail() {
   return (
-    <div className="post__detail">
-      <div className="post__box">
-        <div className="post__title">제목이다</div>
-        <div className="post__user-name">kidrauhl@naver.com</div>
-        <div className="post__date">2023.10.30</div>
+    <div className="detail__box">
+      <div className="detail__title">제목이다</div>
+      <div className="detail__profile-box">
+        <div className="detail__profile"></div>
+        <div className="detail__user-name">kidrauhl@naver.com</div>
+        <div className="detail__date">2023.10.30</div>
+      </div>
+      <div className="detail__utils-box">
+        <div className="detail__delete">삭제</div>
+        <div className="detail__edit">수정</div>
+      </div>
+      <div className="detail__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+        perferendis beatae! Esse placeat perferendis tempore in corrupti, culpa
+        totam perspiciatis ipsam rem magnam, ratione exercitationem maiores quis
+        temporibus quo veritatis.
       </div>
     </div>
   );
