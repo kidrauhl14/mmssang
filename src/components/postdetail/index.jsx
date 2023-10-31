3075bbc6e88e0f287e802078c905f832d2c07daa
Eunchae <ddungal3@gmail.com>
2023-10-31 15:37:00 +0900

    Feat: [PostDetail컴포넌트] 분기처리 (post가 있을 때 / post가 없을 때)

--- src/components/postdetail/index.jsx
+++ src/components/postdetail/index.jsx
@@ -14,10 +14,11 @@ export default function PostDetail() {
 
   const getPost = async (id) => {
     if(id) {
-      const docRef = doc(db, 'posts', id);
+      //getDoc이라는 Firestore메서드를 사용해, 하나의 document를 특정하여 가져옴
+      const docRef = doc(db, "posts", id);
       const docSnap = await getDoc(docRef);
-
-      setPost({id: docSnap.id, ...(docSnap.data())})
+      // id에 따라 해당 데이터를 보여줌
+      setPost({ id: docSnap.id, ...docSnap.data() });
     }
   };
 
@@ -28,24 +29,27 @@ export default function PostDetail() {
   },[params.id]);
 
   return (
-    <div className="detail__box">
-      <div className="detail__title">제목이다</div>
-      <div className="detail__profile-box">
-        <div className="detail__profile"></div>
-        <div className="detail__user-name">kidrauhl@naver.com</div>
-        <div className="detail__date">2023.10.30</div>
-      </div>
-      <div className="detail__utils-box">
-        <div className="detail__delete">삭제</div>
-        <div className="detail__edit">수정</div>
-      </div>
-      <div className="detail__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-        perferendis beatae! Esse placeat perferendis tempore in corrupti, culpa
-        totam perspiciatis ipsam rem magnam, ratione exercitationem maiores quis
-        temporibus quo veritatis.
-      </div>
-    </div>
+    <>
+      {post ? (
+        <div className="detail__box">
+          <div className="detail__title">{post.title}</div>
+          <div className="detail__profile-box">
+            <div className="detail__profile"></div>
+            <div className="detail__user-name">{post.email}</div>
+            <div className="detail__date">{post.createdAt}</div>
+          </div>
+          <div className="detail__utils-box">
+            <div className="detail__delete">삭제</div>
+            <div className="detail__edit">수정</div>
+          </div>
+          <div className="detail__text">
+            {post.content}
+          </div>
+        </div>
+      ) : (
+        <div>로딩중입니당</div>
+      )}
+    </>
   );
 }
 
